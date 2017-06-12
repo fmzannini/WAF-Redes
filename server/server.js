0b02e578e258ca24127b2826fc7fbdaf00d98ee0
Victoria <mvictoria.cavo@gmail.com>
2017-06-12 14:56:48 -0300

    Add front

--- server/server.js
+++ server/server.js
@@ -9,25 +9,183 @@ function serve(ip, port) {
       }
 
       var db = new Sequelize('postgres://postgres:postgres@localhost:5432/mydb', {
-        logging: true
+          logging: false
       });
 
       db.sync().then(function (orm) {
-        http.createServer(function(request, response) {
-          var myparam = "'asd'; SELECT * FROM pg_catalog.pg_tables";
+          http.createServer(function(request, response) {
+            if (request.method == 'GET') {
+                response.writeHeader(200, {"Content-Type": "text/html"});  
+                response.write(html);  
+                response.end();
+            }
+            if (request.method == 'POST') {
+                var body = '';
+                request.on('data', function (data) {
+                    body += data;
+                });
+                request.on('end', function () {
+                    parsedBody = JSON.parse(body);
+                    switch (request.url) {
+                        case '/add_note':
+                            orm.query("INSERT INTO notes(note) values( '" + parsedBody.note + "');").then(function () {
+                                response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+                                response.write(parsedBody.note);
+                                response.end();
+                            });
+                            break;
 
+                        case '/notes':
+                            orm.query('SELECT * FROM notes WHERE note LIKE "%' + parsedBody.search + '%";').then(function (values) {
+                                response.writeHeader(200, {"Content-Type": "application/json"});
+                                response.write(JSON.stringify(values[0], null, 2));
+                                response.end();
+                            });
+                            break;
+                      }
+                });
+            }
 
-          orm.query("insert into mytable values(" + Math.random().toString() + ")").then(function () {
-            orm.query("SELECT * FROM mytable WHERE name=" + myparam + ";").then(function (values) {
-            // orm.query("select * from mytable").then(function (values) {
-              response.writeHeader(200, {"Content-Type": "application/json"});
-              response.write(JSON.stringify(values[0], null, 2));
-              response.end();
-            });
-          });
         }).listen(port, ip);
       });
     });
 }
 
+
+/*
+
+function serve(ip, port) {
+    var db = pgp('postgres://postgres:postgres@localhost:5432/mydb');
+
+    app.listen(ip, port);
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    app.get('/', function(req, res){
+        res.render('server.html');
+    });
+
+    app.post('/add_note', function(req, res){
+        db.none("INSERT INTO notes values(" + req.body.note + ")").then(function () {
+            res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+            res.end();
+        }).catch(function (err) {
+            return next(err);
+        });
+    });
+
+    app.post('/notes', function(req, res){
+        db.many("SELECT * FROM notes WHERE note LIKE '%" + req.body.search + "%'").then(function (values) {
+            res.writeHeader(200, {"Content-Type": "application/json"});
+            res.write(JSON.stringify(values[0], null, 2));
+            res.end();
+        }).catch(function (err) {
+            return next(err);
+        });
+    });
+
+}
+
+*/
+
+
+
+
+
+
+
+/*
+
+
+var http = require('http'),
+    fs = require('fs'),
+    Sequelize = require('sequelize'),
+    querystring = require('querystring');
+
+function processPost(request, response, callback) {
+    var queryData = "";
+    if(typeof callback !== 'function') return null;
+
+    if(request.method == 'POST') {
+        request.on('data', function(data) {
+            queryData += data;
+            if(queryData.length > 1e6) {
+                queryData = "";
+                response.writeHead(413, {'Content-Type': 'text/plain'}).end();
+                request.connection.destroy();
+            }
+        });
+
+        request.on('end', function() {
+            request.post = querystring.parse(queryData);
+            callback();
+        });
+
+    } else {
+        response.writeHead(405, {'Content-Type': 'text/plain'});
+        response.end();
+    }
+}
+
+function serve(ip, port) {
+    fs.readFile('server.html', function (err, html) {
+        if (err) {
+            throw err;
+        }
+
+        var db = new Sequelize('postgres://postgres:postgres@localhost:5432/mydb', {
+            logging: true
+        });
+
+      // db.sync().then(function (orm) {
+      //   http.createServer(function(request, response) {
+      //     var myparam = "'asd'; SELECT * FROM pg_catalog.pg_tables";
+
+
+      //     orm.query("insert into mytable values(" + Math.random().toString() + ")").then(function () {
+      //       orm.query("SELECT * FROM mytable WHERE name=" + myparam + ";").then(function (values) {
+      //       // orm.query("select * from mytable").then(function (values) {
+      //         response.writeHeader(200, {"Content-Type": "application/json"});
+      //         response.write(JSON.stringify(values[0], null, 2));
+      //         response.end();
+      //       });
+      //     });
+      //   }).listen(port, ip);
+      // });
+
+        db.sync().then(function (orm) {
+            http.createServer(function(request, response) {
+                switch (request.url) {
+                    case '/add_note':
+                        processPost(request, response, function() {
+                            orm.query("INSERT INTO notes values(" + request.post.note + ")").then(function () {
+                                response.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+                                response.end();
+                            });
+                        });  
+                        
+                        break;
+
+                    case '/notes':
+                        processPost(request, response, function() {
+                            orm.query("SELECT * FROM notes WHERE note LIKE '%" + request.post.search + "%'").then(function (values) {
+                                response.writeHeader(200, {"Content-Type": "application/json"});
+                                response.write(JSON.stringify(values[0], null, 2));
+                                response.end();
+                            });
+                        });  
+
+                    default:
+                        response.writeHeader(200, {"Content-Type": "text/html"});  
+                        response.write(html);  
+                        response.end();
+                }
+            }).listen(port, ip);
+        });
+    });
+
+}
+
+*/
 serve ("0.0.0.0", 80);
--- server/server.sh
+++ server/server.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env bash
 
-echo -e "-- BEGIN ${HOSTNAME} --\n"
-echo -e "installing node"
-# sudo apt-get install python-software-properties
-# curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-# sudo apt-get install nodejs
+#echo -e "-- BEGIN ${HOSTNAME} --\n"
+#echo -e "installing node"
+#sudo apt-get install python-software-properties
+#curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+#sudo apt-get install nodejs
 
 # postgres
-# sudo apt-get update -y
+#sudo apt-get update -y
 
-# if ! [ -d ~/.rbenv/plugins/ruby-build ]; then
+if ! [ -d ~/.rbenv/plugins/ruby-build ]; then
 
-# vagrant@webserver1:~$ /var/lib/postgresql/9.3/main/^C
+vagrant@webserver1:~$ /var/lib/postgresql/9.3/main/^C
 
-# fi
+fi
 
-# sudo apt-get install libpq-dev postgresql-common postgresql -y
-# sql_command="CREATE ROLE \"postgres\" WITH LOGIN SUPERUSER PASSWORD 'postgres';"
-# sudo sql_command="$sql_command" -Eu postgres bash -c 'psql -c "$sql_command"'
-# sudo db_command="CREATE DATABASE mydb;" -Eu postgres bash -c 'psql -c "$db_command"'
-# sudo db_command="CREATE TABLE mytable(name varchar(64));" -Eu postgres bash -c 'psql -d mydb -c "$db_command"'
+#sudo apt-get install libpq-dev postgresql-common postgresql -y
+sql_command="ALTER ROLE \"postgres\" WITH LOGIN SUPERUSER PASSWORD 'postgres';"
+sudo sql_command="$sql_command" -Eu postgres bash -c 'psql -c "$sql_command"'
+sudo db_command="CREATE DATABASE mydb;" -Eu postgres bash -c 'psql -c "$db_command"'
+sudo db_command="CREATE TABLE notes(note varchar(300));" -Eu postgres bash -c 'psql -d mydb -c "$db_command"'
+sudo db_command="SELECT * FROM notes;" -Eu postgres bash -c 'psql -d mydb -c "$db_command"'
 
 # https://stackoverflow.com/questions/18664074/getting-error-peer-authentication-failed-for-user-postgres-when-trying-to-ge
 
 # sequelize
-# npm i -g pg
-# npm i -g sequelize
-# npm i pg
-# npm i sequelize
+#npm i -g pg
+#npm i -g sequelize
+#npm i pg
+#npm i sequelize
 
 # server
 node server.js
